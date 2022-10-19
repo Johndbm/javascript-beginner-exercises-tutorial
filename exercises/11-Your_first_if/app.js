e1974a088d2294573fbc5b499962980f6c052a5f
john.banks <johndbanksm@gmail.com>
2022-10-19 02:56:24 +0000

    Trying excersise 11, Conditional Statements

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,24 @@
 let total = prompt('How many km are left to go?');
 
 // Your code below:
+function distance(d) {
+    let result;
+    if (d > 100){
+        result = "We still have a bit of driving left to go";
+    } else {
+        result = ""
+    }
+}
+
+function distance(d) {
+    let result;
+    if (d > 100){
+        result = "We still have a bit of driving left to go";
+    } else {
+        if (d >= 50){
+            
+        } else if (d <= 100){
+            result = "We'll be there in 5 minutes";
+        }
+    }
+}
