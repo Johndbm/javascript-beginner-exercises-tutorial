3b01da815b7b9dc0d28cb2c31cf73536106746f6
john.banks <johndbanksm@gmail.com>
2022-11-07 21:58:13 +0000

    got help live on the exercise + finished exercise 11

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,25 +1,14 @@
 let total = prompt('How many km are left to go?');
 
 // Your code below:
-function distance(d) {
-    let result;
-    if (d > 100){
-        result = "We still have a bit of driving left to go";
-    } else {
-        result = ""
-    }
+
+if (total > 100 ){
+    console.log("We still have a bit of driving left to go")
+
+} else if(total > 50 && total <= 100){
+    console.log("We'll be there in 5 minutes")
+} else if (total <= 50){
+    console.log("I'm parking. I'll see you right now")
 }
-/*intentando algo en la estructura del cuerpo de la funcion
-// Esta es la funcion :
-function distance(d) {
-    let result;
-    if (d > 100){
-        result = "We still have a bit of driving left to go";
-    } else {
-        if (d >= 50){
-            
-        } else if (d <= 100){
-            result = "We'll be there in 5 minutes";
-        }
-    }
-}*/
+
+console.log(total)
