5a006f1b40477889f5aefa8a3d110a311fcf7a81
john.banks <johndbanksm@gmail.com>
2022-11-08 22:07:00 +0000

    added exercise 16

--- exercises/16-Your_first_loop/app.js
+++ exercises/16-Your_first_loop/app.js
@@ -1,14 +1,12 @@
 /* Done - check tests and clear for student use */
 
-function startCounting()
-{
+function startCounting(){
 	
-	for(let i = 0; i < 100; i++)
-	{
+	for (let i = 0; i < 101; i++) {
 		console.log(i);
 	}
 	
-	return i;
+	
 }
 
 startCounting();
