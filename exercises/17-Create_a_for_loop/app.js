7eb7cc5870d4e77a535e40a2a0b6e638351b9435
john.banks <johndbanksm@gmail.com>
2022-11-09 02:28:59 +0000

    added exercise 17

--- exercises/17-Create_a_for_loop/app.js
+++ exercises/17-Create_a_for_loop/app.js
@@ -1,8 +1,9 @@
 // Declare and write your function here:
 
 function standardsMaker(){
-for (let i = 1; i < 300; i++){
-    console.log("I will write questions if I'm stuck")
-}
+    for (let i = 1; i <= 300; i++){
+        console.log("I will write questions if I'm stuck");
+        
+    }
 }
 standardsMaker();
